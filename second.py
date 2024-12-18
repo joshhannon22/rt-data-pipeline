17df14c49309649c303fd77c05dab3cebd46d70f
Josh Hannon <joshzh7@gmail.com>
2024-12-18 16:04:40 -0500

    new

--- second.py
+++ second.py
@@ -6,6 +6,9 @@ import pyodbc
 
 with open('stock_data_compact.json', 'r') as file:
     data = json.load(file)
+    
+with open('app-secret.txt', 'r') as file:
+    client_secret = file.read()
 
 symbol = data["Meta Data"]["2. Symbol"]
 time_series_data = data["Time Series (Daily)"]
@@ -18,7 +21,6 @@ server = 'datapipelineserver22.database.windows.net'
 database = 'data-pipeline-db'
 tenant_id = 'b0f5bbda-5dc8-4ac8-8621-fb81b23db439'  # Azure Active Directory tenant ID
 client_id = '9234a681-aeb8-4adc-8a30-6b0831317027'  # Service principal application ID
-client_secret = 'laJ8Q~I6aK8ErXBi4bf.QSstQ2aJCW9.b7edPaJz'  # Service principal secret
 # Create the connection string using Azure AD service principal authentication
 conn_str = f'DRIVER={{ODBC Driver 18 for SQL Server}};SERVER={server};PORT=1433;DATABASE={database};' \
            f'Authentication=ActiveDirectoryServicePrincipal;UID={client_id};PWD={client_secret};' \
